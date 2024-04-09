b02e25c64c37d2e2c407787b8b64db6813ca08ef
Оловиддин Мухаммадиев <muxammadiyev.o@corp.click.uz>
2024-04-09 16:03:51 +0500

    prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from '@app/app.controller'
+import { AppService } from '@app/app.service'
+import { PrismaService } from '@app/prisma.service'
+// import { AuthModule } from '@app/auth/auth.module'
+// import { UserModule } from '@app/user/user.module'
+// import { FileModule } from '@app/file/file.module'
+// import { ProjectModule } from '@app/project/project.module'
+// import { SkillModule } from '@app/skill/skill.module'
+// import { NewsModule } from '@app/news/news.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		// UserModule,
+		// FileModule,
+		// AuthModule,
+		// ProjectModule,
+		// SkillModule,
+		// NewsModule
+	],
+	controllers: [AppController],
+	providers: [AppService, PrismaService]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from '@app/app.module'
+import { PrismaService } from '@app/prisma.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  const prismaService = app.get(PrismaService)
+  // await prismaService.enableShutdownHooks(app)
+
+  app.setGlobalPrefix('api')
+  app.enableCors()
+  await app.listen(process.env.PORT || 5200)
+
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
-bootstrap();
+
+bootstrap()
